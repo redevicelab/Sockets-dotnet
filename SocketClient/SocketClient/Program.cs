20a3bcbbbea0e0ad7b12f10317691b32732f39ce
Redevice <redevice7@gmail.com>
2021-10-24 23:29:34 +0600

    old code

---
+++ SocketClient/SocketClient/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace SocketClient
+{
+    class Program
+    {
+
+
+        private static byte[] GetContent(string fileName)//Метод для перевода содержимого файла в байты тут все просто
+        {
+            byte[] Content = null;
+            using (FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read))
+            {
+                Content = new byte[fs.Length];
+                fs.Read(Content, 0, (int)fs.Length);
+            }
+            return Content;
+        }
+
+        // адрес и порт сервера, к которому будем подключаться
+        static int port = 8005; // порт сервера
+        static string address = "127.0.0.1"; // адрес сервера
+        static void Main(string[] args)
+        {
+            try
+            {
+                IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse(address), port);
+
+                Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                // подключаемся к удаленному хосту
+                socket.Connect(ipPoint);
+                string path; //Путь для указания где лежит файл
+                Console.Write("Введите адрес файла: ");
+                path = $@"{Console.ReadLine()}";//Тут пользователь вводит путь
+
+
+                MemoryStream ms = new MemoryStream();//Делаем поток для записи в него имя файла и содержимого
+                var bytesContent = GetContent(path);//Тут получаем содержимое файла в байтах
+                var bytesFileName = new byte[100];//Задаем переменную для хранения имени файла
+                byte[] dataSend = null;//Массив байт для отправки файла и его названия
+                var bFIleName = Encoding.UTF8.GetBytes(Path.GetFileName(path));//Тут получаем имя файла в байтах
+                ms.Write(bFIleName, 0, bFIleName.Length);//Записываем в поток имя файла
+                ms.Position = bytesFileName.Length;//Смещаем положение в потоке на длину названия файла
+                ms.Write(bytesContent, 0, bytesContent.Length);//Записываем в поток содержимае файла
+                dataSend = ms.ToArray();//записываем все в массив для отпрвавки
+
+                socket.Send(dataSend);//отправляем все на сервер
+                // закрываем сокет
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+                Console.WriteLine("Файл отправлен поток закрыт");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ SocketServer/SocketServer/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Collections.Generic;
+
+namespace SocketServer
+{
+    class Program
+    {
+        static int port = 8005; // порт для приема входящих запросов
+        static void Main(string[] args)
+        {
+            // получаем адреса для запуска сокета
+            IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), port);
+
+            // создаем сокет
+            Socket listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                // связываем сокет с локальной точкой, по которой будем принимать данные
+                listenSocket.Bind(ipPoint);
+
+                // начинаем прослушивание
+                listenSocket.Listen(10);
+                Console.Write("Введите папке для получания файла:");
+                string path = $"{Console.ReadLine()}";
+                Console.WriteLine("Сервер запущен. Ожидание подключений...");
+
+                while (true)
+                {
+                    Socket handler = listenSocket.Accept();
+                    // получаем сообщение
+                    int bytes = 0; // количество полученных байтов
+                    byte[] data = new byte[256]; // буфер для получаемых данных
+                    MemoryStream ms = new MemoryStream();//Поток для получение имени файла и содержимого
+                    ms.Position = 0;//Позиция откуда начать запись
+                    do
+                    {//В цикле читаем данные пока они есть и записываем в поток
+                        bytes = handler.Receive(data);
+                        ms.Write(data, 0, data.Length);
+                    }
+                    while (handler.Available > 0);
+                    ms.Seek(0, SeekOrigin.Begin);//Тут указываем позицию откуда будем читать
+                    var fileNameByte = new byte[100];//Переменная дла имени файла в байтах
+                    ms.Read(fileNameByte, 0, fileNameByte.Length-1);//чттаем имя файла
+                    string fileNameString = Encoding.ASCII.GetString(fileNameByte).Replace("\0", string.Empty);//переводим имя файла из байт в строку
+                    ms.Position = 100;//указывем позицию откуда будем читать содержимое файла
+                    byte[] content = new byte[ms.Length-fileNameByte.Length];//Делаем буфер для хранения содержимого файла
+                    ms.Read(content, 0, (int)ms.Length - fileNameByte.Length);//Читаем в буфер содержимое файла
+                    File.WriteAllBytes(path + "\\" +fileNameString, content);//Тут уже сохраняем файл в папку 
+                    Console.WriteLine("Файл получен и сохранен!!!");
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
